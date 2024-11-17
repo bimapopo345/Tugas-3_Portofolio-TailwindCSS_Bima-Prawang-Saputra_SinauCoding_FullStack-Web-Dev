c479a78bd9aaf2c6983cc0c6b7e71e73ee4c55e6
bimapopo345 <bimapopo345@gmail.com>
2024-11-17 23:33:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#f1c40f",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
